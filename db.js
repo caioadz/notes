12698daf5280666a63f16e3089400d245b7989aa
Caio Zem <caioadz@hotmail.com>
2018-03-06 13:12:39 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 /* Some data to be returned by the controllers */
 
 module.exports.notes = [
-	{ id: 0, title: 'Node.js', text: 'Try learning by examples' },
-	{ id: 1, title: 'MongoDb', text: 'Remember to learn MongoDb soon after' },
-	{ id: 2, title: 'Bootstrap', text: 'It will help you to build your interface' },
-	{ id: 3, title: 'Jade', text: 'HTML templates make your life easier' }
+	{ id: 0, title: 'Node.js', text: 'Node.js is an open-source, cross-platform JavaScript run-time environment for executing JavaScript code server-side.' },
+	{ id: 1, title: 'MongoDb', text: 'MongoDB is one of several database types to arise in the mid-2000s under the NoSQL banner. Instead of using tables and rows as in relational databases, MongoDB is built on an architecture of collections and documents. Documents comprise sets of key-value pairs and are the basic unit of data in MongoDB.' },
+	{ id: 2, title: 'Bootstrap', text: 'Bootstrap is a free and open-source front-end library for designing websites and web applications.' },
+	{ id: 3, title: 'Jade', text: 'Jade is an templating engine, primarily used for server-side templating in NodeJS.' }
 ]
